648ba1c44ff285d4ae4dc4a4690a4eb6047670f8
SungbinYang <ysb05222@naver.com>
2023-01-29 15:50:41 +0900

    자바를 시작하기 전에

---
+++ javaEssence/src/main/java/me/sungbin/ch01/Hello.java
@@ -0,0 +1,18 @@
+package me.sungbin.ch01;
+
+/**
+ * @author : rovert
+ * @packageName : me.sungbin.ch01
+ * @fileName : Hello
+ * @date : 2023/01/29
+ * @description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2023/01/29       rovert         최초 생성
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
