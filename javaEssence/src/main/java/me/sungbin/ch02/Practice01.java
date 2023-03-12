1790f9ad15ffe439b77527c5f6a04ff0d5de1d76
SungbinYang <ysb05222@naver.com>
2023-03-12 15:15:00 +0900

    연습문제

---
+++ javaEssence/src/main/java/me/sungbin/ch02/Practice01.java
@@ -0,0 +1,21 @@
+package me.sungbin.ch02;
+
+/**
+ * @author : rovert
+ * @packageName : me.sungbin.ch02
+ * @fileName : Practice01
+ * @date : 2023/03/12
+ * @description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2023/03/12       rovert         최초 생성
+ */
+public class Practice01 {
+    public static void main(String[] args) {
+        // 자신의 주민번호를 출력
+        long regNo = 1234551234567L;
+
+        System.out.println("regNo = " + regNo);
+    }
+}
